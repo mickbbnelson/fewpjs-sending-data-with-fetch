c79b5cfe4455e799849d8979b05f9c19c07cfceb
mickbbnelson <75447451+mickbbnelson@users.noreply.github.com>
2021-09-07 08:44:17 -0400

    commit tree 269f711ef6330c121b4712707b495a045a12f2ac

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Add your code here
+const body = document.querySelector("body")
+
+function submitData(name, email){
+    
+
+    const confObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email
+        })
+    };
+
+    return fetch("http://localhost:3000/users", confObj)
+    .then(function(response) {
+        return response.json();
+      })
+      .then(function(object) {
+        return DOMId(object);
+      })
+      .catch(function(error) {
+        body.innerHTML += error.message
+      })
+}
+
+function DOMId(object){
+body.innerHTML += `<p>${object.id}</p>`
+}
